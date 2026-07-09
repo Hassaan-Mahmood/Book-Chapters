2a0707119f6526dc2b217644cf455cbc0ac79909
Hassaan-Mahmood <hassaan2980@gmail.com>
2026-07-09 12:41:35 -0700

    initial

---
+++ app.js
@@ -0,0 +1,27 @@
+// ========== Chapter 24 ========== //
+// var firstName = "Hassaan";
+// var LastName = "Mahmood";
+// var fullName = firstName + " " + LastName;
+
+// var firstChar = firstName.slice(0, 1);
+// var firstChar = firstName.charAt(0);
+// console.log(firstChar);
+// var LastChar = fullName.charAt(fullName.length -1);
+// console.log(LastChar);
+
+// let text = "Hello World! This is a test string.";
+// for (let i = 0; i < text.length; i++) {
+//     if (text.charAt(i) === "!") {
+//         console.log("Exclamation mark found at index: " + i);
+//         break;
+//     }
+// }
+
+// ========== Chapter 25 ========== //
+
+
+
+
+
+
+
